fb1a6fa62468737d633a7bdc45f39cda8a9563eb
Raz Muhammad <p200023@pwr.nu.edu.pk>
2024-12-10 17:37:01 +0500

    Added Rect

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('public'));
 app.get('/user', (req, res) => {
   res.json({ data: { name: 'Raz' } });
 });
 
+app.use('/', (res, res) => {
+  res.sendFile(path.join(__dirname, 'public/dist/index.html'));
+});
 app.listen(3000, () => {
   console.log('Listening...');
 });
